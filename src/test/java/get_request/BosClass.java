b8c820f0f11db1b60d954879c486e6b11121d07f
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-01-06 15:13:51 +0300

    v11

--- src/test/java/get_request/BosClass.java
+++ src/test/java/get_request/BosClass.java
@@ -1,13 +1,10 @@
 package get_request;
 
-import base_urls.GoRestBaseUrl;
-import io.restassured.response.Response;
-import org.junit.Test;
-import test_data.BosClassTestData;
-
-import java.util.HashMap;
-import java.util.Map;
-
+import base_urls.*;
+import io.restassured.response.*;
+import org.junit.*;
+import test_data.*;
+import java.util.*;
 import static io.restassured.RestAssured.*;
 import static org.junit.Assert.*;
 
